51e1c5cea9b5c6f621f2a3725fe199f860e1611c
legator-admin <hello@legatorsolutions.com>
2021-06-07 22:07:13 +0530

    funding page added

--- src/components/funding/funding.js
+++ src/components/funding/funding.js
@@ -32,7 +32,86 @@ const Funding_cmp = ({data}) => {
 
     console.log(gen_data)
 
+
+    // Sankey data preparation 
+    
+    
+    // const s_groupby_funders =  Array.from(d3.group(data, d => d.funder_shortname), ([key, value]) => ({ key, value })) 
+    // const s_groupby_startYr =  Array.from(d3.group(data, d => d.YearStart), ([key, value]) => ({ key, value }))
+    // const s_groupby_endYr =  Array.from(d3.group(data, d => d.YearEnd), ([key, value]) => ({ key, value }))
+    // const s_groupby_category =  Array.from(d3.group(data, d => d.Category), ([key, value]) => ({ key, value }))
+
+    // const s_funders = s_groupby_funders.map(a => a.key);
+    // const s_StartYear = s_groupby_startYr.map(a => a.key);
+    // const s_EndYear = s_groupby_endYr.map(a => a.key);
+    // const s_Category = s_groupby_category.map(a => a.key);
+
+    // const S_nodes = s_funders.concat(s_StartYear,s_EndYear,s_Category)
+    
+    // const Sankey_nodes = S_nodes.map(d=>{
+    //     return{
+    //         name: d
+    //     }
+    // })
+    let Sankey_Links = []
+
+    data.forEach(d => {
+        Sankey_Links.push({
+            source: d.funder_shortname,
+            target: d.YearStart,
+            value : d.Amount
+        })
+        Sankey_Links.push({
+            source: d.YearStart,
+            target: d.YearEnd,
+            value : d.Amount
+        })
+        Sankey_Links.push({
+            source: d.YearEnd,
+            target: d.Category,
+            value : d.Amount
+        })
+        Sankey_Links.push({
+            source: d.Category,
+            target: d.Title,
+            value : d.Amount
+        })
+        // Sankey_Links.push({
+        //     source: d.Category,
+        //     target: d.PIs,
+        //     value : d.Amount
+        // })
+        // Sankey_Links.push({
+        //     source: d.funder_shortname,
+        //     target: d.YearStart,
+        //     value : d.Amount
+        // })
+        // Sankey_Links.push({
+        //     source: d.YearStart,
+        //     target: d.YearEnd,
+        //     value : d.Amount
+        // })
+        // Sankey_Links.push({
+        //     source: d.YearEnd,
+        //     target: d.Category,
+        //     value : d.Amount
+        // })
+    });
+
+    const tmp_nodes = Sankey_Links.map(d => d.source).concat(Sankey_Links.map(d => d.target))
+    const noDup_nodes = [...new Set(tmp_nodes)]
+    const Sankey_nodes = noDup_nodes.map(d=>{
+        return{
+            name:d
+        }
+    })
+
+
+    console.log(Sankey_nodes)
+    console.log(Sankey_Links)
+
     return (
+        <>
         <Segment>
             <ReactECharts style={{height:500}}
                 option={
@@ -42,9 +121,30 @@ const Funding_cmp = ({data}) => {
                             type: 'treemap',
                             visibleMin: 300,
                             label: {
-                                show: true,
-                                formatter: '{b}'
+                                normal: {
+                                    position: 'insideTopLeft',
+                                    formatter: function (params) {
+                                        var arr = [
+                                            params.name,
+                                            'Fund $'+params.value+'M'
+                                        ];
+                
+                                        // mode !== 1 && arr.push(
+                                        //     '{household|$ ' + echarts.format.addCommas((+params.value[3].toFixed(4)) * 1000) + '} {label|per household}'
+                                        // );
+                
+                                        return (
+                                            params.name + ' Found: $'+params.value+'M'
+                                        )
+                                               
+                                                
+                                    }
+                                }
                             },
+                            // label: {
+                            //     show: true,
+                            //     formatter: '{b}'
+                            // },
                             upperLabel: {
                                 show: true,
                                 height: 30
@@ -54,34 +154,36 @@ const Funding_cmp = ({data}) => {
                             },
                             levels: getLevelOption(),
                             data: gen_data
-                            // data: [{
-                            //     name: 'nodeA',            // First tree
-                            //     value: 10,
-                            //     children: [{
-                            //         name: 'nodeAa',       // First leaf of first tree
-                            //         value: 4
-                            //     }, {
-                            //         name: 'nodeAb',       // Second leaf of first tree
-                            //         value: 6
-                            //     }]
-                            // }, {
-                            //     name: 'nodeB',            // Second tree
-                            //     value: 20,
-                            //     children: [{
-                            //         name: 'nodeBa',       // Son of first tree
-                            //         value: 20,
-                            //         children: [{
-                            //             name: 'nodeBa1',  // Granson of first tree
-                            //             value: 20
-                            //         }]
-                            //     }]
-                            // }]
+                            
                         }]
                     }
                 }
             />
 
         </Segment>
+
+        <Segment>
+            <ReactECharts style={{height:700}}
+                    option={
+                        {
+                            tooltip: {
+                                trigger: 'item',
+                                triggerOn: 'mousemove'
+                            },
+                            series: {
+                                type: 'sankey',
+                                layout: 'none',
+                                emphasis: {
+                                    focus: 'adjacency'
+                                },
+                                data: Sankey_nodes,
+                                links: Sankey_Links
+                            }
+                        }
+                    }
+            />
+        </Segment>
+        </>
     )
 }
 
