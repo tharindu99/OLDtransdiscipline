26f3e7e95b8b6e454406a0b0fea1322ba322adda
legator-admin <hello@legatorsolutions.com>
2021-10-08 18:03:23 +0530

    cleanup

--- src/components/publication/publication.js
+++ src/components/publication/publication.js
@@ -5,10 +5,7 @@ import Publication_list from './publication_list'
 
 
 const Publication_cmp = ({data}) =>{
-    // const groupByYear = groupBy('Year');
-    // const publication_groupby_year = groupByYear(data)
     const publication_groupby_year = Array.from (new Map([...d3.group(data, d => d.Year).entries()].sort()))
-   
     return(
         <Segment>
             <Publication_list data={publication_groupby_year.reverse()}></Publication_list>
@@ -16,12 +13,4 @@ const Publication_cmp = ({data}) =>{
     )
 }
 
-
 export default Publication_cmp
-
-const groupBy = key => array =>
-  array.reduce((objectsByKeyValue, obj) => {
-    const value = obj[key];
-    objectsByKeyValue[value] = (objectsByKeyValue[value] || []).concat(obj);
-    return objectsByKeyValue;
-  }, {});
